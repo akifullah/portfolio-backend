f8f0ea8c19e7f034a370e3e7e9704e83084f814d
Akif Ullah <akifullah0317@gmail.com>
2024-01-19 08:26:04 +0500

    Cors origin

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ Connection();
 const uplaod = path.join(process.cwd(), "uploads")
 app.use(express.static(uplaod))
 app.use(express.json())
-app.use(cors("http://localhost:3000"))
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // ROUTES
 app.use(projectRouter)
