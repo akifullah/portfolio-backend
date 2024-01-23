c8082f28a51347d377b01af00f60c29b948caa2c
Akif Ullah <akifullah0317@gmail.com>
2024-01-23 09:29:57 +0500

    Header Origin

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ const uplaod = path.join(process.cwd(), "uploads")
 app.use(express.static(uplaod))
 app.use((req, res, next) => {
   // res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*')
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.header(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
