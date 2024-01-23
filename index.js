d2fbf3f021f1347f2e6fd94075ebb2739495d3ed
Akif Ullah <akifullah0317@gmail.com>
2024-01-23 09:50:58 +0500

    Content type

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use((req, res, next) => {
 res.setHeader("Access-Control-Allow-Credentials", "true");
 
 res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-res.setHeader("Access-Control-Allow-Headers", 
+res.setHeader("Access-Control-Allow-Headers",
+res.setHeader("Content-Type", "Application/json") ,
 "Access-Control-Allow-Origin",
-"Content-Type",
  "Access-Control-Allow-Headers Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   })
