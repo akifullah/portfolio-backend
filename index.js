37df0562c1fc628ec306187ba16a0e30eab4d080
Akif Ullah <akifullah0317@gmail.com>
2024-01-23 09:25:25 +0500

    Origin

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ app.use(express.static(uplaod))
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
