a6f6cd416c5072270c670e4aacfb6f6d652d7d72
Akif Ullah <akifullah0317@gmail.com>
2024-01-23 08:10:16 +0500

    Cors

--- index.js
+++ index.js
@@ -18,9 +18,15 @@ Connection();
 const uplaod = path.join(process.cwd(), "uploads")
 app.use(express.static(uplaod))
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
     next();
   })
 app.use(express.json())
